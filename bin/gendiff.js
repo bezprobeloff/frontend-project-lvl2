e6a90e04bfd03473d68eaaf443cb665ffc85e429
bezprobeloff <bezprobeloff@gmail.com>
2020-08-26 15:37:26 +0300

    Добавлена опция -а

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,7 @@ const programm = new Command();
 programm.version('0.0.1');
 programm.description('Compares two configuration files and shows a difference.');
 
+programm
+  .option('-f, --format', 'select format type');
+
 programm.parse(process.argv);
